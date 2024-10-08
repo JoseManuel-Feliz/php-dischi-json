37597a0d72a5eda429070be6fb1005b99e9b7eb7
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-10-08 18:52:30 +0200

    import axios and vue.js, created vue.js instance

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,13 @@
 console.log('JS OK');
+// IMPORT AXIOS
+const axios = require('axios/dist/browser/axios.cjs');
+
+//VUE INSTANCE
+import { createApp } from 'vue'
+
+createApp({
+    data() {
+        return {
+        }
+    }
+}).mount('#app')
